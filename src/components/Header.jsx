94133863d29c24f8ee3a22306252aab01f3d0c6b
ainurotanovna <atoxanbayeva.ml@gmail.com>
2025-04-03 21:51:48 +0500

    origin

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOUB5kxGbdsk2jUMN89LM8iPJAB1w1UOqvTQ&s" alt="Logo" className="logoImg"/>
+        </div>
+        )
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Profile = () => {
+    return ( <div className="content">
+            <div>
+                <img src = 'https://vetandlife.ru/wp-content/uploads/2022/11/becca-_r6w0R6SueQ-unsplash.jpg' />
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+            </div>
+            <div>
+                New Post
+            </div>
+            <div>
+                post 1
+            </div>
+            <div>
+                post 2
+            </div>
+        </div>
+        )
+}
+export default Profile;
